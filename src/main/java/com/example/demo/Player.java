3d6f385b951f40abf36104e0aa20699e50f178d2
Pjotrx <44673973+Pjotrx@users.noreply.github.com>
2022-10-27 09:25:01 +0200

    added Player class

---
+++ src/main/java/com/example/demo/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private String playerName;
+
+    public Player(String playerName){
+        this.playerName = playerName;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+    public void setName(String name) {
+        this.playerName = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerName='" + playerName + '\'' +
+                '}';
+    }
+}
