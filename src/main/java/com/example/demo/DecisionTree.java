eb77a1a57a91a479decc5fdeaf01310035154d22
Pjotr Kooijmans <kooijmans97@gmail.com>
2022-11-06 12:20:35 +0100

    Changed getNextMove() return type and structure.

--- src/main/java/com/example/demo/DecisionTree.java
+++ src/main/java/com/example/demo/DecisionTree.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
 public class DecisionTree {
     Board board;
     Stones stone;
@@ -13,92 +11,78 @@ public class DecisionTree {
         this.board = board;
         this.stone = stone;
     }
-
-
-    public int[] get_next_move(){
+    public int getNextMove(){
+        /*
+        returns the next move as an int from 1 to 9
+         */
         //System.out.println("Starting decision");
+        int piece_counter = this.board.pieceCounter();
         int[] next_move = new int[2];
 
-        if (winOrDefend()[0] != -1){
-            return winOrDefend();
-        }
-
-        //first round
-        if(this.board.pieceCounter() == 0){   //first move, so place piece in corner
-            next_move = corners[3];
-        } else if(this.board.pieceCounter() == 1){ // second move, place in middle
-            if(this.board.getBoard()[1][1].getValue() == ' '){
-                next_move = middle;
-            } else{next_move = corners[3];}
-
-        }
-
-        //second round
-        else if(this.board.pieceCounter() == 2){                                 //third move, check if opponent in middle
-            if(this.board.getBoard()[1][1].getValue() == ' '){              //if middle is open
-
-
-                if(this.board.getBoard()[2][0].getValue() == ' '){
-                    next_move = corners[2];
-                } else {next_move = corners[1];}
-
-            } else {    //if middle is chosen by opponent, go diagonally across
-                next_move = corners[0];
-            }
-        } else if (this.board.pieceCounter() == 3) {    //fourth move, place on side
-            for(int[] side : sides){
-                if(this.board.getBoard()[side[0]][side[1]].getValue() == ' '){
-                    next_move =side;
-                }
-            }
-        }
-
-
-        //third round
-        else if(this.board.pieceCounter() == 4){ //fifth move
-            for(int[] corner : corners){                                //check for empty corner
-                if(this.board.getBoard()[corner[0]][corner[1]].getValue() == ' '){ //found empty corner
-                    next_move = corner;
+        if (winOrDefend()[0] != -1){                                                            //check if there is a possibility of winning or losing.
+            next_move = winOrDefend();
+        } else {
+            switch (piece_counter){
+                case 0:
+                    next_move = corners[3];                                                     //first move, so place piece in corner
                     break;
-                }
-            }
-        }
+                case 1:
+                    if(this.board.getBoard()[1][1].getValue() == ' '){
+                        next_move = middle;                                                     //second move, place in the middle. If that's not possible we can play as if we went first
+                    } else{next_move = corners[3];}
+                    break;
+                case 2:
+                    if(this.board.getBoard()[1][1].getValue() == ' '){                          //if middle is open
+                        if(this.board.getBoard()[2][0].getValue() == ' '){                      //choose corner 7 or 3
+                            next_move = corners[2];
+                        } else {next_move = corners[1];}
 
-        //if all else fails
-        else if (this.board.pieceCounter() > 4){
-            for (int i = 0; i < this.board.getSize(); i++){
-                for (int j = 0; j < this.board.getSize(); j++){
-                    if(this.board.getBoard()[i][j].getValue() == ' '){
-                        int[] move = {i, j};
-                        next_move = move;
+                    } else {                                                                    //if middle is chosen by opponent, go diagonally across
+                        next_move = corners[0];
                     }
-                }
+                    break;
+                case 3:                                                                         //fourth move, place on side
+                    for(int[] side : sides){
+                        if(this.board.getBoard()[side[0]][side[1]].getValue() == ' '){
+                            next_move =side;
+                        }
+                    }
+                    break;
+                case 4:
+                    for(int[] corner : corners){                                                //check for empty corner
+                        if(this.board.getBoard()[corner[0]][corner[1]].getValue() == ' '){      //found empty corner
+                            next_move = corner;
+                            break;
+                        }
+                    }
+                    break;
+                default:                                                                        //if all else fails, pick a random empty place on the board. Should never be reached.
+                    for (int i = 0; i < this.board.getSize(); i++){
+                        for (int j = 0; j < this.board.getSize(); j++){
+                            if(this.board.getBoard()[i][j].getValue() == ' '){
+                                next_move = new int[]{i, j};
+                            }
+                        }
+                    }
             }
         }
-
-
-
-
         //System.out.println("next move is: " + Arrays.toString(next_move));
-        return next_move;
+        return convertToBoardPosition(next_move);
     }
 
-
-
-
     private char[][] createArrays(){
-        char[][] boardArrays = new char[8][];
+        /*
+        create an array for each possible winning combination of places on the board filled with the pieces on the board.
+         */
+        char[][] boardArrays = new char[8][3];
         int index = 0;
         for (int i = 0; i < this.board.getSize(); i++){
-
             char[] column = new char[3];
             char[] row = new char[3];
             for (int j = 0; j < this.board.getSize(); j ++){
                 column[j] = this.board.getBoard()[j][i].getValue();
                 row[j] = this.board.getBoard()[i][j].getValue();
             }
-
-
             boardArrays[index] = column;
             index++;
             boardArrays[index] = row;
@@ -114,17 +98,19 @@ public class DecisionTree {
                 this.board.getBoard()[1][1].getValue(),
                 this.board.getBoard()[2][0].getValue()
         };
-
-
         boardArrays[6] = diagonal1;
         boardArrays[7] = diagonal2;
-
-
         return boardArrays;
     }
 
     private int[] checkDoubles(char[][] arrays){
-        boolean foundFlag = false;
+        /*
+        check if there are any arrays with two of the same char in it and if there is an empty space.
+        If there is more than one array that satisfies this condition, return the one where char == stone.getValue() to win.
+
+
+         */
+        boolean isFound = false;
         int index = 0;
         int [][] doubles = new int[8][3];
         int[] foundDouble = new int[1];
@@ -135,12 +121,11 @@ public class DecisionTree {
                     if (arrays[i][j] == arrays[i][k] && arrays[i][j] != ' '){
                         //double
                         if (spaceInROw(i, j, k, arrays)){
-                            foundFlag = true;
+                            isFound = true;
                             //System.out.println("found double at " +i+ ", "+ j +", " + k);
                             foundDouble = new int[] {i,j,k};
                             doubles[index] = foundDouble;
                             index++;
-                            //return foundDouble; //winning needs to take precedence over defendding.
                         }
 
                     }
@@ -148,61 +133,54 @@ public class DecisionTree {
             }
         }
 
-        if(foundFlag){
+        if(isFound){
             //System.out.println(Arrays.deepToString(doubles));
             for (int[] doub : doubles){
                 if(doub[1] == stone.getValue()){foundDouble = doub;}
             }
             return foundDouble;
         }
-        int[] temp = {-1,-1};
-        return temp;
+        return new int[]{-1,-1};
     }
 
     private Boolean spaceInROw(int i, int j, int k, char[][] arrays){
+        /*
+        check if one of the elements in a row is empty
+         */
         if (j + k == 1 && arrays[i][2] == ' '){
             return true;
         } else if (j +k  == 2 && arrays[i][1] == ' ' ){
             return true;
-        } else if(j + k == 3 && arrays[i][0] == ' ') {
-            return true;
-        }
-        return false;
+        } else return j + k == 3 && arrays[i][0] == ' ';
     }
+
+
     private int[] winOrDefend(){
-        char [][] arrays = createArrays();
-        int[] checkDoubles = checkDoubles(arrays);
+        /*
+            check if there are two pieces of the same type next to each other on the board,
+            if so, return the place on the board that prevents or completes three in a row.
+     */
+
+        int[] checkDoubles = checkDoubles(createArrays());
         int[] place_at = new int[2];
         if(checkDoubles[0] != -1){
             int sum = checkDoubles[1] + checkDoubles[2];
             switch(checkDoubles[0]){
-                case -1:
-                    //found no doubles
-                    break;
+
                 case 0:
                     //0
                     if (sum == 1){ //place at index 2
                         place_at[0] = 2;
-                        place_at[1] = 0;
                     }else if (sum == 2){ // place at index 1
                         place_at[0] = 1;
-                        place_at[1] = 0;
-                    }else{ //place at index 0
-                        place_at[0] = 0;
-                        place_at[1] = 0;
                     }
                     break;
                 case 1:
                     //3
                     if (sum == 1){ //place at index 2
-                        place_at[0] = 0;
                         place_at[1] = 2;
                     }else if (sum == 2){ // place at index 1
-                        place_at[0] = 0;
                         place_at[1] = 1;
-                    }else{ //place at index 0
-                        place_at[0] = 0;
-                        place_at[1] = 0;
                     }
                     break;
                 case 2:
@@ -214,7 +192,6 @@ public class DecisionTree {
                         place_at[0] = 1;
                         place_at[1] = 1;
                     }else{ //place at index 0
-                        place_at[0] = 0;
                         place_at[1] = 1;
                     }
                     break;
@@ -228,7 +205,6 @@ public class DecisionTree {
                         place_at[1] = 1;
                     }else{ //place at index 0
                         place_at[0] = 1;
-                        place_at[1] = 0;
                     }
                     break;
                 case 4:
@@ -240,7 +216,6 @@ public class DecisionTree {
                         place_at[0] = 1;
                         place_at[1] = 2;
                     }else{ //place at index 0
-                        place_at[0] = 0;
                         place_at[1] = 2;
                     }
                     break;
@@ -254,7 +229,6 @@ public class DecisionTree {
                         place_at[1] = 1;
                     }else{ //place at index 0
                         place_at[0] = 2;
-                        place_at[1] = 0;
                     }
                     break;
                 case 6:
@@ -265,33 +239,50 @@ public class DecisionTree {
                     }else if (sum == 2){ // place at index 1
                         place_at[0] = 1;
                         place_at[1] = 1;
-                    }else{ //place at index 0
-                        place_at[0] = 0;
-                        place_at[1] = 0;
                     }
                     break;
                 case 7:
                     if (sum == 1){ //place at index 2
                         place_at[0] = 2;
-                        place_at[1] = 0;
                     }else if (sum == 2){ // place at index 1
                         place_at[0] = 1;
                         place_at[1] = 1;
                     }else{ //place at index 0
-                        place_at[0] = 0;
                         place_at[1] = 2;
                     }
                     //7
                     break;
-
             }
         } else {
             place_at[0] = -1;
             place_at[1] = -1;
         }
-
-
-
         return place_at;
+    }
+
+
+    private int convertToBoardPosition(int[] move){
+        /*
+    Converts the position on the 2d board array to an integer from 1 to 9.
+     */
+        int result = 0;
+        if(move[0] == 0){
+            if (move[1] == 0){result = 1;}
+            else if (move[1] == 1) {result = 2;}/*
+    Converts the position on the 2d board array to an integer from 1 to 9.
+     */
+            else{result = 3;}
+        } //1, 2, 3
+        else if (move[0] == 1){
+            if (move[1] == 0){result = 4;}
+            else if (move[1] == 1) {result = 5;}
+            else{result = 6;}
+        } //4, 5, 6
+        else if (move[0] == 2){
+            if (move[1] == 0){result = 7;}
+            else if (move[1] == 1) {result = 8;}
+            else{result = 9;}
+        } //7, 8, 9
+        return result;
     }
 }
